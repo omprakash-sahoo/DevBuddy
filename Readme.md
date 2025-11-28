eb8ae48186e85ed0dd49c8a8d52f452474332051
Om Prakash Sahoo <omprakashsahoo.bdk@gmail.com>
2025-11-29 00:09:34 +0530

    readme file and rout in app.js

---
+++ Readme.md
@@ -0,0 +1,4 @@
+installed express
+install nodemon globally: npm i -g nodemon
+config the package.json for nodemon: "start": "node src/app.js", "dev": "nodemon src/app.js"
+added the rout for api
--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
 const express = require("express");
 
 const app = express();
-app.use((req, res) => {
-  res.send("Hello from cas");
+app.use("/", (req, res) => {
+  res.send("This home page");
+});
+app.use("/about", (req, res) => {
+  res.send("This about page");
 });
 app.listen(3001, () => {
   console.log("Server is running");
