48b0d445685898dfba6214a7cd4b789bd597213e
Om Prakash Sahoo <omprakashsahoo.bdk@gmail.com>
2025-11-29 22:57:17 +0530

    Routing- order matters

--- src/app.js
+++ src/app.js
@@ -1,12 +1,13 @@
 const express = require("express");
 
 const app = express();
-app.use("/", (req, res) => {
-  res.send("This home page");
-});
 app.use("/about", (req, res) => {
   res.send("This about page");
+});
+app.use("/", (req, res) => {
+  res.send("This home page");
 });
 app.listen(3001, () => {
   console.log("Server is running");
 });
+//Order of the rout matter if / will keep in top any routs will redirect to /
