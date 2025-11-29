528e3c62cd32b80b6f4be8300e10e2ca094b1747
Om Prakash Sahoo <omprakashsahoo.bdk@gmail.com>
2025-11-29 23:32:03 +0530

    Dynamic routing

--- src/app.js
+++ src/app.js
@@ -7,12 +7,27 @@ const app = express();
 // app.use("/", (req, res) => {
 //   res.send("This home page");
 // });
-app.get("/about", (req, res) => {
-  res.send("Hello");
+// app.get("/about", (req, res) => {
+//   res.send("Hello");
+// });
+// app.post("/post", (req, res) => {
+//   res.send("This is post call");
+// });
+
+// ================DYNAMIC ROUTING=================
+
+// http://localhost:3001/about/12/sahoo
+app.post("/about/:user_id/:name", (req, res) => {
+  console.log(req.params);
+  res.send(req.params);
 });
-app.post("/post", (req, res) => {
-  res.send("This is post call");
+
+//http://localhost:3001/contact?hello=12&hi=12
+app.post("/contact", (req, res) => {
+  console.log(req.query);
+  res.send(req.query);
 });
+
 app.listen(3001, () => {
   console.log("Server is running");
 });
