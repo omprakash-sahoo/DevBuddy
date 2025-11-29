ef8d866a6abea05cbd806c3cfcb4c661940b86d1
Om Prakash Sahoo <omprakashsahoo.bdk@gmail.com>
2025-11-29 23:09:19 +0530

    Get and post request

--- src/app.js
+++ src/app.js
@@ -1,11 +1,17 @@
 const express = require("express");
 
 const app = express();
-app.use("/about", (req, res) => {
-  res.send("This about page");
+// app.use("/about", (req, res) => {
+//   res.send("This about page");
+// });
+// app.use("/", (req, res) => {
+//   res.send("This home page");
+// });
+app.get("/about", (req, res) => {
+  res.send("Hello");
 });
-app.use("/", (req, res) => {
-  res.send("This home page");
+app.post("/post", (req, res) => {
+  res.send("This is post call");
 });
 app.listen(3001, () => {
   console.log("Server is running");
