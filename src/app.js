9ff0f494c6730b91e76ff3173a9a967783fd0f54
Om Prakash Sahoo <omprakashsahoo.bdk@gmail.com>
2025-11-30 23:10:51 +0530

    MIDDLEWARE

--- src/app.js
+++ src/app.js
@@ -17,18 +17,36 @@ const app = express();
 // ================DYNAMIC ROUTING=================
 
 // http://localhost:3001/about/12/sahoo
-app.post("/about/:user_id/:name", (req, res) => {
-  console.log(req.params);
-  res.send(req.params);
-});
+// app.post("/about/:user_id/:name", (req, res) => {
+//   console.log(req.params);
+//   res.send(req.params);
+// });
 
 //http://localhost:3001/contact?hello=12&hi=12
-app.post("/contact", (req, res) => {
-  console.log(req.query);
-  res.send(req.query);
-});
+// app.post("/contact", (req, res) => {
+//   console.log(req.query);
+//   res.send(req.query);
+// });
+
+// =============== MIDDLEWARE =================
+
+app.get(
+  "/user",
+  (req, res, next) => {
+    console.log("Handler 1");
+    next(); // go to next handler
+  },
+  (req, res, next) => {
+    console.log("Handler 2");
+    next(); // go to next
+  },
+  (req, res) => {
+    res.send("Final Response");
+  }
+);
 
 app.listen(3001, () => {
   console.log("Server is running");
 });
+//
 //Order of the rout matter if / will keep in top any routs will redirect to /
